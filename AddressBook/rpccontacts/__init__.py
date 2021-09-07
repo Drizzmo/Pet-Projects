4e28d0baefd367e92994e776fd226c565110edd2
Drizzmo <50156365+Drizzmo@users.noreply.github.com>
2021-09-06 22:45:10 -0400

    Add files via upload

---
+++ AddressBook/rpccontacts/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""This module provides the rpcontacts package."""
+__version__ = '0.1.0.0'
+
+# dont need any code in __init__ file, just used to
+# maintain version of application
+
